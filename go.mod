804cb03f598e843a785d772418ff19d04985d664
Alex Efros <powerman@powerman.name>
2019-05-09 22:13:43 +0300

    feat: add config file, closes #1

--- go.mod
+++ go.mod
@@ -1,7 +1,10 @@
 module github.com/powerman/gh-make-labels
 
+go 1.12
+
 require (
 	github.com/google/go-github v17.0.0+incompatible
 	github.com/google/go-querystring v1.0.0 // indirect
-	gopkg.in/yaml.v2 v2.2.1
+	github.com/powerman/structlog v0.3.0
+	gopkg.in/yaml.v2 v2.2.2
 )
--- main.go
+++ main.go
@@ -2,133 +2,213 @@ package main
 
 import (
 	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"os"
+	"path"
+	"runtime"
 	"strings"
 
 	"github.com/google/go-github/github"
-
+	"github.com/powerman/structlog"
 	"gopkg.in/yaml.v2"
 )
 
-var makeLabels = map[string]string{
-	"bug":          "e11d21",
-	"enhancement":  "009800",
-	"question":     "0052cc",
-	"API":          "bfe5bf",
-	"architecture": "fef2c0",
-	"chore":        "fbca04",
-	"doc":          "fbca04",
-	"feature":      "009800",
-	"optimization": "009800",
-	"framework":    "fef2c0",
-	"production":   "d4c5f9",
-	"refactoring":  "fbca04",
-	"security":     "e11d21",
-	"staging":      "d4c5f9",
-	"TBD":          "ffffff",
-	"test":         "fbca04",
-	"URGENT":       "f7c6c7",
-	"User Story":   "bfe5bf",
-	"WIP":          "ffffff",
-	"★★★":          "5319e7",
-	"★★☆":          "5319e7",
-	"★☆☆":          "5319e7",
-	"◷0d1h":        "207de5",
-	"◷0d3h":        "207de5",
-	"◷1d":          "207de5",
-	"◷2d":          "207de5",
-	"◷4d":          "207de5",
-}
-
-var destructive = flag.Bool("d", false, "delete unknown labels")
+//nolint:gochecknoglobals
+var (
+	cmd = strings.TrimSuffix(path.Base(os.Args[0]), ".test")
+	ver string // set by ./build
+	log = structlog.NewZeroLogger().
+		SetPrefixKeys(structlog.KeyLevel).
+		SetKeysFormat(map[string]string{
+			structlog.KeyLevel:   "%[2]s",
+			structlog.KeyMessage: " %#[2]q",
+			"err":                " %s: %s",
+		})
+	cfg struct {
+		version    bool
+		logLevel   string
+		configPath string
+		cleanup    bool
+		owner      string
+		repo       string
+	}
+)
 
 func main() {
-	owner, repo := parseArgs()
-	user, token := loadHubCfg()
-	ctx := context.Background()
-
-	client := github.NewClient((&github.BasicAuthTransport{
-		Username: user,
-		Password: token,
-	}).Client())
-
-	labels, _, err := client.Issues.ListLabels(ctx, owner, repo, nil)
-	if err != nil {
-		log.Fatal(err)
-	}
-	for _, l := range labels {
-		if makeLabels[*l.Name] == "" {
-			if !*destructive {
-				log.Printf("ignore %q", l)
-			} else {
-				log.Printf("delete %q", l)
-				_, err := client.Issues.DeleteLabel(ctx, owner, repo, *l.Name)
-				if err != nil {
-					log.Fatal(err)
-				}
-			}
-		} else if makeLabels[*l.Name] != *l.Color {
-			log.Printf("colour %q (%s to %s)", l, *l.Color, makeLabels[*l.Name])
-			*l.Color = makeLabels[*l.Name]
-			_, _, err := client.Issues.EditLabel(ctx, owner, repo, *l.Name, l)
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Printf("accept %q", l)
-		}
-		delete(makeLabels, *l.Name)
-	}
-	for name, color := range makeLabels {
-		log.Printf("create %q (%s)", name, color)
-		l := &github.Label{Name: &name, Color: &color}
-		_, _, err := client.Issues.CreateLabel(ctx, owner, repo, l)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-}
-
-func parseArgs() (owner, repo string) {
+	flag.StringVar(&cfg.logLevel, "log.level", "info", "log `level` (debug|info|warn|err)")
+	flag.StringVar(&cfg.configPath, "config", "gh-labels.yml", "`path` to config file")
+	flag.BoolVar(&cfg.cleanup, "cleanup", false, "delete unknown labels")
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s [flags] owner/repo\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "Usage: %s [flags] owner/repo\n", cmd)
 		flag.PrintDefaults()
 	}
 	flag.Parse()
-	if flag.NArg() != 1 || strings.Count(flag.Arg(0), "/") != 1 {
+	if flag.NArg() == 1 && strings.Count(flag.Arg(0), "/") == 1 {
+		ownerRepo := strings.Split(flag.Arg(0), "/")
+		cfg.owner, cfg.repo = ownerRepo[0], ownerRepo[1]
+	}
+
+	switch {
+	case cfg.owner == "" || cfg.repo == "":
 		flag.Usage()
 		os.Exit(2)
+	case cfg.version: // Must be checked after all other flags for ease testing.
+		fmt.Println(cmd, ver, runtime.Version())
+		os.Exit(0)
 	}
-	ownerRepo := strings.Split(flag.Arg(0), "/")
-	return ownerRepo[0], ownerRepo[1]
+
+	// Wrong log.level is not fatal, it will be reported and set to "debug".
+	log.SetLogLevel(structlog.ParseLevel(cfg.logLevel))
+
+	labels, err := loadLabelsCfg(cfg.configPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	user, token, err := loadHubCfg()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	err = makeGitHubLabels(ctx, user, token, cfg.owner, cfg.repo, labels, cfg.cleanup)
+	if err != nil {
+		os.Exit(1)
+	}
 }
 
-func loadHubCfg() (user, token string) {
+func loadLabelsCfg(path string) (map[string]string, error) {
+	buf, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var labelCfg struct {
+		Labels map[string]string
+		Colors map[string]string
+	}
+	err = yaml.Unmarshal(buf, &labelCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	labels := make(map[string]string, len(labelCfg.Labels))
+	for label, colorName := range labelCfg.Labels {
+		colorHex, ok := labelCfg.Colors[colorName]
+		if !ok {
+			return nil, fmt.Errorf("label %q: missing color %q", label, colorName)
+		}
+		labels[label] = colorHex
+	}
+	return labels, nil
+}
+
+func loadHubCfg() (user, token string, err error) {
+	buf, err := ioutil.ReadFile(os.Getenv("HOME") + "/.config/hub")
+	if err != nil {
+		if os.IsNotExist(err) {
+			return "", "", errors.New("hub tool is not installed or not configured")
+		}
+		return "", "", err
+	}
+
 	var hubCfg = make(map[string][]struct {
 		Protocol   string
 		User       string
 		OAuthToken string `yaml:"oauth_token"`
 	})
-
-	hubCfgData, err := ioutil.ReadFile(os.Getenv("HOME") + "/.config/hub")
-	if os.IsNotExist(err) {
-		log.Fatal("hub tool is not installed or configured")
-	}
+	err = yaml.Unmarshal(buf, &hubCfg)
 	if err != nil {
-		log.Fatal(err)
-	}
-
-	err = yaml.Unmarshal([]byte(hubCfgData), &hubCfg)
-	if err != nil {
-		log.Fatal(err)
+		return "", "", err
 	}
 	if len(hubCfg["github.com"]) == 0 {
-		log.Fatal("failed to detect configuration of hub tool")
+		return "", "", errors.New("failed to detect configuration of hub tool")
 	}
 
-	return hubCfg["github.com"][0].User, hubCfg["github.com"][0].OAuthToken
+	return hubCfg["github.com"][0].User, hubCfg["github.com"][0].OAuthToken, nil
+}
+
+func makeGitHubLabels(
+	ctx context.Context,
+	user string,
+	pass string,
+	owner string,
+	repo string,
+	newLabels map[string]string,
+	cleanup bool,
+) error {
+	auth := &github.BasicAuthTransport{Username: user, Password: pass}
+	client := github.NewClient(auth.Client())
+
+	var oldLabels []*github.Label
+	const perPage = 100
+	for page := 1; ; page++ {
+		opt := &github.ListOptions{Page: page, PerPage: perPage}
+		labels, _, err := client.Issues.ListLabels(ctx, owner, repo, opt)
+		if err != nil {
+			return log.Err("failed to list labels", "err", prettify(err))
+		}
+		oldLabels = append(oldLabels, labels...)
+		if len(labels) < perPage {
+			break
+		}
+	}
+
+	var err error
+	for _, l := range oldLabels {
+		label, color := *l.Name, *l.Color
+		if newLabels[label] == "" {
+			if !cleanup {
+				log.Debug("ignore", "label", label)
+			} else {
+				_, err = client.Issues.DeleteLabel(ctx, owner, repo, label)
+				if err != nil {
+					log.Warn("failed to remove", "label", label, "err", prettify(err))
+				} else {
+					log.Info("remove", "label", label)
+				}
+			}
+		} else if newLabels[label] != color {
+			*l.Color = newLabels[label]
+			_, _, err = client.Issues.EditLabel(ctx, owner, repo, label, l)
+			if err != nil {
+				log.Warn("failed to update", "label", label, "color", newLabels[label], "err", prettify(err))
+			} else {
+				log.Info("update", "label", label, "color", newLabels[label], "old_color", color)
+			}
+		} else {
+			log.Debug("exists", "label", label)
+		}
+		delete(newLabels, label)
+	}
+
+	for label, color := range newLabels {
+		l := &github.Label{Name: &label, Color: &color}
+		_, _, err = client.Issues.CreateLabel(ctx, owner, repo, l)
+		if err != nil {
+			log.Warn("failed to create", "label", label, "color", color, "err", prettify(err))
+		} else {
+			log.Info("create", "label", label, "color", color)
+		}
+	}
+
+	return err
+}
+
+func prettify(err error) error {
+	switch err := err.(type) {
+	default:
+		return err
+	case *github.ErrorResponse:
+		if len(err.Errors) != 1 {
+			return err
+		} else if err.Errors[0].Code == "custom" {
+			return errors.New(err.Errors[0].Message)
+		} else {
+			return errors.New(err.Errors[0].Code)
+		}
+	}
 }
